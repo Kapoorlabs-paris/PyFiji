3e0a99ca6e932c14fd6364ad7b3addc96725fab0
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-01-22 12:26:02 +0100

    lock

--- pyfiji/methods.py
+++ pyfiji/methods.py
@@ -7,10 +7,13 @@ Created on Sat Jan 22 11:27:36 2022
 """
 from tifffile import imread
 import pandas as pd
-from scipy.ndimage import sobel, median_filter
+from scipy.ndimage import  median_filter
 from skimage.morphology import skeletonize
 from skimage.measure import regionprops
 import os
+from skimage.segmentation import find_boundaries
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
 def kymo_mtrack_export(fname, savedir, median_radius = 2):
     
@@ -19,7 +22,7 @@ def kymo_mtrack_export(fname, savedir, median_radius = 2):
     #Apply median filter to smooth the image
     median_image = median_filter(image, size = median_radius)
     #Apply sobel to get the edge image
-    edge_image = sobel(median_image)
+    edge_image = find_boundaries(median_image)
     #Create a pixel level thich binary image
     skeleton_image = skeletonize(edge_image)
     #Use region props for getting the properties of the binary image
@@ -28,8 +31,20 @@ def kymo_mtrack_export(fname, savedir, median_radius = 2):
     coordinates_lt = [prop.coords for prop in properties]
     #Sort the coordinates by time
     coordinates_lt = sorted(coordinates_lt, key=lambda k: k[1])
-        
+    doubleplot(image, skeleton_image, 'original_kymograph', 'for_mtrack_export')    
     df = pd.dataframe(coordinates_lt, columns = ['Length', 'Time'])
     #Save the data as Mtrack readable text file
     df.to_csv(savedir + '/' + Name + 'Mtrack' +  '.csv')          
       
+    
+def doubleplot(imageA, imageB, titleA, titleB):
+    fig, axes = plt.subplots(1, 2, figsize=(15, 6))
+    ax = axes.ravel()
+    ax[0].imshow(imageA, cmap=cm.Spectral)
+    ax[0].set_title(titleA)
+    
+    ax[1].imshow(imageB, cmap=cm.Spectral)
+    ax[1].set_title(titleB)
+    
+    plt.tight_layout()
+    plt.show()   
